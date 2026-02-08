fc4d25dbbf3ca6d2fb44db846ea859db4e8e9032
chickenandstats <chicken@chickenandstats.com>
2026-02-08 15:16:14 -0600

    Update team.py
    Adding docstring to the Team class object

--- src/chickenstats/chicken_nhl/team.py
+++ src/chickenstats/chicken_nhl/team.py
@@ -2,24 +2,50 @@ from io import BytesIO
 from PIL import Image, ImageFile
 
 from chickenstats.utilities import ChickenSession
-from chickenstats.chicken_nhl import Season
 from chickenstats.chicken_nhl._info import NHL_COLORS, team_names, alt_team_codes, team_codes, INTERNATIONAL_COLORS
-
-import pandas as pd
-import polars as pl
-
-from typing import Literal
 
 
 class Team:
-    """Class instance for team information, including team name, code, and colors."""
+    """Class instance for team information, including team name, code, and colors.
 
-    def __init__(
-        self,
-        team_code: str | None = None,
-        team_name: str | None = None,
-        backend: Literal["polars", "pandas"] = "polars",
-    ) -> None:
+    Parameters:
+        team_code (str | None):
+            Three-letter team code, e.g., NSH
+        team_name: (str | None):
+            Team's full name, e.g., NASHVILLE PREDATORS
+
+    Attributes:
+        team_code (str):
+            Three-letter team code, e.g., NSH
+        team_code_alt (str):
+            Alternate team code, if it exists e.g., T.B. vs. TBL, PHX vs. ARI
+        team_name (str):
+            Full team name, e.g., NASHVILLE PREDATORS
+        colors (dict):
+            Dictionary of team colors, to be used in charts. Primary and secondary colors
+            are mapped to "GOAL" and "SHOT", respectively. "MISS" is a gray used for everyone
+        primary_color (str):
+            Team's primary color, also mapped to self.colors["GOAL"]
+        secondary_color (str):
+            Team's secondary color, also mapped to self.colors["SHOT"]
+        tertiary_color (str):
+            Gray used for eall teams, also mapped to self.colors["MISS"]
+        logo_url (str):
+            URL to download logo from chickenstats GitHub repo.
+
+    Examples:
+        Get team colors as a dictionary:
+        >>> from chickenstats.chicken_nhl import Team
+        >>> team = Team(team_code="NSH")
+        >>> team.colors
+
+        Get team logo as an ImageFile:
+        >>> from chickenstats.chicken_nhl import Team
+        >>> team = Team(team_code="NASHVILLE PREDATORS")
+        >>> team.logo
+    """
+
+    def __init__(self, team_code: str | None = None, team_name: str | None = None) -> None:
         """Instantiates team information, including team name, code, and colors."""
         if not team_code and not team_name:
             raise ValueError("Either team code or team name must be provided.")
@@ -66,11 +92,18 @@ class Team:
         url_stem = "https://raw.githubusercontent.com/chickenandstats/chickenstats/refs/heads/main/logos"
         self.logo_url = f"{url_stem}/{folder_stem}/{team_code}.png"
 
-        self._game_ids: list | None = None
-
     @property
     def logo(self) -> ImageFile.ImageFile:
-        """Fetch logo from chickenstats GitHub repo."""
+        """Fetch logo from chickenstats GitHub repo.
+
+        Returns logo as an ImageFile
+
+        Examples:
+            >>> from chickenstats.chicken_nhl import Team
+            >>> team = Team(team_code="NSH")
+            >>> team.logo
+
+        """
         with ChickenSession() as session:
             logo = BytesIO(session.get(self.logo_url).content)
 
