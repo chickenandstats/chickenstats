e21910352148046a0f2ab0145a097d6ced006436
chickenandstats <chicken@chickenandstats.com>
2026-02-10 12:40:29 -0600

    Update process_data.py
    Updating for type hints

--- xg_model/process_data.py
+++ xg_model/process_data.py
@@ -1,3 +1,4 @@
+from rich.progress import TaskID
 from pathlib import Path
 
 import polars as pl
@@ -14,37 +15,37 @@ def main():
     and ./processed directories.
     """
     # The years we want to process
-    years = list(range(2024, 2009, -1))
+    years: list[int] = list(range(2024, 2009, -1))
 
     # Empty lists to collect the data after we prep each year individually
-    evens = []
-    powerplays = []
-    shorthandeds = []
-    empty_fors = []
-    empty_againsts = []
+    evens: list[pl.DataFrame] = []
+    powerplays: list[pl.DataFrame] = []
+    shorthandeds: list[pl.DataFrame] = []
+    empty_fors: list[pl.DataFrame] = []
+    empty_againsts: list[pl.DataFrame] = []
 
     # Setting up the progress bar to track everything
     with ChickenProgress(speed_estimate_period=300, transient=True) as progress:
         # Creating task to track everything
-        pbar_message = "Prepping data for the xG experiments..."
-        progress_task = progress.add_task(pbar_message, total=len(years))
+        pbar_message: str = "Prepping data for the xG experiments..."
+        progress_task: TaskID = progress.add_task(pbar_message, total=len(years))
 
         # Iterating through the years, keeping track of our place with idx for the progress bar
         for idx, year in enumerate(years):
             # Updating the progress bar message initially, no need to advance
-            pbar_message = f"Prepping {year}..."
+            pbar_message: str = f"Prepping {year}..."
             progress.update(progress_task, description=pbar_message, advance=False, refresh=True)
 
             # Loading data to prep
-            filepath = Path.cwd() / f"raw/pbp_{year}.csv"
-            pbp = pl.read_csv(filepath)
+            filepath: Path = Path.cwd() / f"raw/pbp_{year}.csv"
+            pbp: pl.DataFrame = pl.read_csv(filepath, infer_schema_length=1_000_000)
 
             # Prepping the various dataframes
-            even = prep_data_polars(pbp, "even")
-            powerplay = prep_data_polars(pbp, "powerplay")
-            shorthanded = prep_data_polars(pbp, "shorthanded")
-            empty_for = prep_data_polars(pbp, "empty_for")
-            empty_against = prep_data_polars(pbp, "empty_against")
+            even: pl.DataFrame = prep_data_polars(pbp, "even")
+            powerplay: pl.DataFrame = prep_data_polars(pbp, "powerplay")
+            shorthanded: pl.DataFrame = prep_data_polars(pbp, "shorthanded")
+            empty_for: pl.DataFrame = prep_data_polars(pbp, "empty_for")
+            empty_against: pl.DataFrame = prep_data_polars(pbp, "empty_against")
 
             if year != 2024:
                 # If it isn't the hold out year, we're appending to the empty lists above
@@ -56,7 +57,7 @@ def main():
 
             else:
                 # If it is 2024, then we're saving to a hold out
-                hold_out_directory = Path.cwd() / "hold_out"
+                hold_out_directory: Path = Path.cwd() / "hold_out"
 
                 # Creating the directory if it doesn't exist
                 if not hold_out_directory.exists():
@@ -70,30 +71,30 @@ def main():
                 empty_against.write_csv(hold_out_directory / "empty_against.csv")
 
             if idx == len(years) - 1:
-                pbar_message = "Finished prepping data"
+                pbar_message: str = "Finished prepping data"
 
             progress.update(progress_task, description=pbar_message, advance=1, refresh=True)
 
     # Concatenating the data before saving it
-    even = pl.concat(evens)
-    powerplay = pl.concat(powerplays)
-    shorthanded = pl.concat(shorthandeds)
-    empty_for = pl.concat(empty_fors)
-    empty_against = pl.concat(empty_againsts)
+    even: pl.DataFrame = pl.concat(evens, how="diagonal")
+    powerplay: pl.DataFrame = pl.concat(powerplays, how="diagonal")
+    shorthanded: pl.DataFrame = pl.concat(shorthandeds, how="diagonal")
+    empty_for: pl.DataFrame = pl.concat(empty_fors, how="diagonal")
+    empty_against: pl.DataFrame = pl.concat(empty_againsts, how="diagonal")
 
     # Setting the directory for the processed data
-    processed_directory = Path.cwd() / "processed"
+    processed_directory: Path = Path.cwd() / "processed"
 
     # Creating directory if it doesn't exist
     if not processed_directory.exists():
         processed_directory.mkdir()
 
     # Saving everything
-    even.to_csv(processed_directory / "even_strength.csv")
-    powerplay.to_csv(processed_directory / "powerplay.csv")
-    shorthanded.to_csv(processed_directory / "shorthanded.csv")
-    empty_for.to_csv(processed_directory / "empty_for.csv")
-    empty_against.to_csv(processed_directory / "empty_against.csv")
+    even.write_csv(processed_directory / "even_strength.csv")
+    powerplay.write_csv(processed_directory / "powerplay.csv")
+    shorthanded.write_csv(processed_directory / "shorthanded.csv")
+    empty_for.write_csv(processed_directory / "empty_for.csv")
+    empty_against.write_csv(processed_directory / "empty_against.csv")
 
 
 if __name__ == "__main__":
