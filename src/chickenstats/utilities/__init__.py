eb5a7b36c5a4b6225878b6ed528db4f43cef594d
chickenandstats <chicken@chickenandstats.com>
2026-04-01 13:47:58 -0500

    refactor: adding utility functions to __init__
    Adding data_directory and charts_directory functions to chickenstats/utilities/__init__.py

--- src/chickenstats/utilities/__init__.py
+++ src/chickenstats/utilities/__init__.py
@@ -2,9 +2,11 @@ from chickenstats.utilities.utilities import (
     ChickenProgress,
     ChickenProgressIndeterminate,
     ChickenSession,
+    data_directory,
+    charts_directory,
     add_cs_mplstyles,
 )
 
 add_cs_mplstyles()
 
-__all__ = ["ChickenProgress", "ChickenProgressIndeterminate", "ChickenSession", "add_cs_mplstyles"]
+__all__ = ["ChickenProgress", "ChickenProgressIndeterminate", "ChickenSession", "data_directory", "charts_directory", "add_cs_mplstyles"]
